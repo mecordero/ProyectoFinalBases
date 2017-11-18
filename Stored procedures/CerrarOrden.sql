89d4b04136e4591e224949d45599b3f74ef139db
Meli Cordero <melisacorderoarias@hotmail.com>
2017-11-18 12:57:19 -0600

    Add files via upload

---
+++ Stored procedures/CerrarOrden.sql
@@ -0,0 +1,28 @@
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+CREATE PROCEDURE CerrarOrden
+	-- Add the parameters for the stored procedure here
+	@id_orden int,
+	@respuesta int OUTPUT
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	UPDATE ORDEN
+	SET ORDEN.status = 3
+	WHERE
+	(SELECT count(*)
+	from
+	OrdenesQueCerrar o
+	WHERE o.id_orden = @id_orden) > 0 AND ORDEN.id_orden = @id_orden
+	SELECT @respuesta = @@ROWCOUNT
+
+END
+GO
