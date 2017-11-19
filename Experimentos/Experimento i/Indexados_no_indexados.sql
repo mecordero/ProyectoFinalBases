320e774986ff9e9d476c0825d18708ebc7a5df24
Meli Cordero <melisacorderoarias@hotmail.com>
2017-11-18 20:25:10 -0600

    Cambié la medicion del tiempo

--- Experimentos/Experimento i/Indexados_no_indexados.sql
+++ Experimentos/Experimento i/Indexados_no_indexados.sql
@@ -1,21 +1,19 @@
--- consulta sobre campos indexados en tabla ORDEN
 
---dura 0ms
-set statistics time on
+DECLARE @tiempo datetime
+SET @tiempo = GETDATE()
 
-SELECT o.id_orden, o.lote, o.id_producto, o.id_linea, o.fecha_creacion, o.fecha_finalizacion, o.status, o.id_copia_flujo
+
+
+SELECT o.id_orden, o.lote, o.id_producto, o.id_linea, o.fecha_creacion, 
+	o.fecha_finalizacion, o.status, o.id_copia_flujo
 FROM ORDEN o
-WHERE o.id_orden = 23190
-
-set statistics time off
-
---consulta sobre campos no indexados
-
---dura 3ms
-set statistics time on
-SELECT o.id_orden, o.lote, o.id_producto, o.id_linea, o.fecha_creacion, o.fecha_finalizacion, o.status, o.id_copia_flujo
-FROM ORDEN o
-WHERE lote = 'CR17K07001'
-set statistics time off
+WHERE lote = 'CR17K07003'
 
 
+
+SELECT DATEDIFF(millisecond, @tiempo, getdate())
+
+
+--create index i1 on ORDEN (lote)
+
+--drop index i1 on orden
--- Experimentos/Experimento j/2 joins.sql
+++ Experimentos/Experimento j/2 joins.sql
@@ -1,12 +1,15 @@
+DECLARE @tiempo datetime
+SET @tiempo = GETDATE()
 
-set statistics time on
 SELECT *
 FROM Producto p
 INNER JOIN ORDEN o
 on o.id_producto = p.id_producto
 INNER JOIN LINEA l
 on l.id_linea = o.id_linea
-set statistics time off
+
+
+SELECT DATEDIFF(millisecond, @tiempo, getdate())
 
 --create index i1 on ORDEN (id_producto)
 --create index i2 on ORDEN (id_linea)
