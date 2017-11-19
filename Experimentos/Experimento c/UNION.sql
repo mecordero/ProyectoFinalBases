000323b0a4e9316599c0a8dc06aa2ecc6d4d03f6
Meli Cordero <melisacorderoarias@hotmail.com>
2017-11-18 21:22:08 -0600

    Delete UNION.sql

--- Experimentos/Experimento c/UNION.sql
+++
@@ -1,8 +0,0 @@
-
---Union de actividades y ad hoc
-
-SELECT 'Actividad' as Tipo,a.nombre, a.codigo, a.descripcion, a.tipo_firma, a.rol_seguridad
-FROM ACTIVIDAD a
-UNION
-SELECT 'Adhoc',ah.nombre, ah.codigo, ah.descripcion, ah.tipo_firma, ah.rol_seguridad
-FROM AD_HOC ah
